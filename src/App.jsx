ead76428347c9638320cb535b018991af1d310f2
Virendra Sirture <virendrasirture05@gmail.com>
2025-01-03 14:36:45 +0530

    routing, tailwind, daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <h1>Hello world</h1>
-  )
+    <>
+      <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body/>}>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/profile" element={<Profile/>} />
+        </Route>
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
