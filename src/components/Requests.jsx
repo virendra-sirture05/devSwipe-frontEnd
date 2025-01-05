7f9cab44ce0d44ddfca3dff6478c8ff7a390a018
Virendra Sirture <virendrasirture05@gmail.com>
2025-01-05 17:29:49 +0530

    feat : show connection requests

---
+++ src/components/Requests.jsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import BASE_URL from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { addRequests } from "../utils/slice/requestSlice";
+
+const Requests = () => {
+    const dispatch = useDispatch();
+    const requests = useSelector(state=>state.requests);
+    console.log(requests);
+
+  const fetchRequests = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/request/received", {
+        withCredentials: true,
+      });
+      dispatch(addRequests(res.data.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  if (!requests) return;
+  if (requests.length === 0) return <h1> No Requests Found</h1>;
+
+  return (
+    <div className="text-center my-10">
+      <h1 className="text-bold text-white text-3xl">Connection Requests</h1>
+      {requests.map((request) => {
+        const { _id, firstName, lastName, photoUrl, age, gender, About } =
+          request.fromUserId;
+        return (
+          <div
+            key={_id}
+            className=" flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/3 mx-auto"
+          >
+            <div>
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full"
+                src={photoUrl}
+              />
+            </div>
+            <div className="text-left mx-4 ">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+              {age && gender && <p>{age + ", " + gender}</p>}
+              <p>{About}</p>
+            </div>
+            <div>
+              <button className="btn btn-primary mx-2">Reject</button>
+              <button className="btn btn-secondary mx-2">Accept</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Requests;
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './slice/userSlice';
 import feedReducer from './slice/feedSlice'
 import connectionReducer from './slice/connectionSlice';
+import requestReducer from './slice/requestSlice';
 const appStore = configureStore({
     reducer:{
         user : userReducer,
         feed : feedReducer,
         connection : connectionReducer,
+        requests : requestReducer,
     }
 })
 
