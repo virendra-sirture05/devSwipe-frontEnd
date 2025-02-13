6d19deacfbd8beaa00b985ab80df089e6760f8e4
Virendra Sirture <virendrasirture05@gmail.com>
2025-02-13 15:15:57 +0530

    nginx proxy

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:3000"
+// const BASE_URL = "http://localhost:3000"
+const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" : "/api";
 
 export default BASE_URL
