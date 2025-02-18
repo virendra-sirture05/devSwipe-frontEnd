f792c9c3a6b4254082f6ceaa4eef33fc02b442c2
Virendra Sirture <virendrasirture05@gmail.com>
2025-02-18 16:29:09 +0530

    some change

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:3000"
-const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+// const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 
-// const BASE_URL = "/api"
+const BASE_URL = "/api"
 export default BASE_URL
